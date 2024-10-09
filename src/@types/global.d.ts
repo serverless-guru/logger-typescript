b93a6e785ef9bde99787abc4ed035932ad3542ed
Daniel Muller <daniel.muller@serverlessguru.com>
2024-10-09 14:10:17 +0100

    feat: Add env vars for fine grained control

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -1,6 +1,10 @@
 declare namespace NodeJS {
     export interface ProcessEnv {
-        LOG_EVENT?: string;
-        LOG_MASK?: string;
+        SG_LOGGER_LOG_EVENT?: string;
+        SG_LOGGER_MASK?: string;
+        SG_LOGGER_MAX_SIZE?: string;
+        SG_LOGGER_COMPRESS_SIZE?: string;
+        SG_LOGGER_NO_COMPRESS?: string;
+        SG_LOGGER_NO_SKIP?: string;
     }
 }
